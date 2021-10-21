f367f8c6ef7fc6ce8cc988bd699e10c230e57ec1
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-22 02:01:20 +0300

    hw15

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
 const userArr = [1, 2, 3, -1, -2, -3];
-
 function positiveArr(arr) {
   const newArr = [];
-  if (arr.length != 0) {
-    console.log(arr);
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > 0) {
-        newArr.push(arr[i]);
-      } else if (arr[i] < 0) {
-        newArr.push();
-      }
-    }
-    if (newArr.length != 0) {
-      console.log(newArr);
-    } else {
-      alert("the NEW array is empty");
-    }
-  } else {
-    alert("the array is empty");
+  if (arr.length === 0) return "the array is empty";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) newArr.push(arr[i]);
   }
+  return newArr.length > 0 ? newArr : null;
 }
-
-positiveArr(userArr);
+console.log(positiveArr(userArr));
